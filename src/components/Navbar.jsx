1c76892f0a2b1421716f6ecbb228ca6b8b038d34
Kevin Babu <116744347+kevin-babu-dotcom@users.noreply.github.com>
2025-06-28 17:58:10 +0530

    navbar fix trial-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import FancyButton from "./fancybutton";
 const Navbar = () => (
   <div className=" h-full justify-between flex w-full">
     <nav className="sticky bg-white md:m-0 hover:shadow-[0_0_30px_rgba(255,255,255,0.6)] shadow-sm md:text-base border py-3 px-3 sm:px-8 md:rounded-3xl">
-    <div className=" mx-auto flex bg-white  flex-col sm:flex-row items-center justify-between rounded-full">
+    <div className=" mx-auto flex bg-white  flex-col sm:flex-row items-center  rounded-full">
       <div className="mb-0 sm:mb-0" >
           <FancyButton to="/" className="px-6 py-4 border-2 border-black bg-orange-300 rounded-full text-black text-xl no-underline font-gilroy">
           <span className="italic font-medium ">KEVIN&nbsp;</span>
